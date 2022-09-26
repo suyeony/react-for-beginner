e11febeec92450b5733a7fbad8b5b04c655f7897
yangsuyeon <suyeonyang03@gmail.com>
2022-09-26 15:50:15 +0900

    Revert "fix updates"
    This reverts commit 04e8f3bc21d3915fe56a78a9d7d74589b9a6db9a.

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -29,7 +29,7 @@ function Search() {
   library.add(faMagnifyingGlass);
 
   const imgHandleError = (e) => {
-    console.log("image not found replace image entered");
+    console.log("image not found");
     e.target.src = notFound;
   };
 
